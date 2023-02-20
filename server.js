d8a146284298f00e0505b42b7dc56818582e818e
Anisha Daniels <anisha.daniels@student.moringaschool.com>
2023-02-20 10:34:41 +0300

    modified server.js deleted .env

--- server.js
+++ server.js
@@ -2,39 +2,38 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
-const config = require('./_config');
+const dotenv = require("dotenv");
+dotenv.config();
 
 // Define routes
 let index = require('./routes/index');
 let image = require('./routes/image');
 
+// // connecting the database
+console.log(process.env.MONGODB_URI)
+async function connectdb() {
+    await mongoose
+      .connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        console.log("Connected to MongoDB");
+      })
+      .catch((err) => {
+        console.log("Error connecting to MongoDB", err.message);
+      });
+  }
+
 // Initializing the app
 const app = express();
-
-// connecting the database
-
-const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  },(err)=>{
-    if (err) {
-        console.log(err)
-    }else{
-        console.log(`Connected to Database: ${MONGODB_URI}`)
-    }
-});
-
-// test if the database has connected successfully
-// let db = mongoose.connection;
-// db.once('open', ()=>{
-//     console.log('Database connected successfully')
-// })
-
 
 
 
 // View Engine
 app.set('view engine', 'ejs');
 
-// Set up the public folder;
+ // Set up the public folder;
 app.use(express.static(path.join(__dirname, 'public')));
 
 // body parser middleware
@@ -45,12 +44,11 @@ app.use('/', index);
 app.use('/image', image);
 
 
+const PORT = process.env.PORT || 8000;
 
- 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT,() =>{
+app.listen(PORT, async () =>{
     console.log(`Server is listening at http://localhost:${PORT}`)
+    await connectdb();
 });
-
 
 module.exports = app;
