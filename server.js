a100ae3731c5ca3b1898effc8e528cdccada8560
Anisha Daniels <anisha.daniels@student.moringaschool.com>
2023-02-22 21:52:38 +0300

    updated server.js

--- server.js
+++ server.js
@@ -2,38 +2,39 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
-const dotenv = require("dotenv");
-dotenv.config();
+const config = require('./_config');
 
 // Define routes
 let index = require('./routes/index');
 let image = require('./routes/image');
 
-// // connecting the database
-
-async function connectdb() {
-    await mongoose
-      .connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-      .then(() => {
-        console.log("Connected to MongoDB");
-      })
-      .catch((err) => {
-        console.log("Error connecting to MongoDB", err.message);
-      });
-  }
-
 // Initializing the app
 const app = express();
+
+// connecting the database
+
+const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  },(err)=>{
+    if (err) {
+        console.log(err)
+    }else{
+        console.log(`Connected to Database: ${MONGODB_URI}`)
+    }
+});
+
+// test if the database has connected successfully
+// let db = mongoose.connection;
+// db.once('open', ()=>{
+//     console.log('Database connected successfully')
+// })
+
 
 
 
 // View Engine
 app.set('view engine', 'ejs');
 
- // Set up the public folder;
+// Set up the public folder;
 app.use(express.static(path.join(__dirname, 'public')));
 
 // body parser middleware
@@ -44,11 +45,12 @@ app.use('/', index);
 app.use('/image', image);
 
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, async () =>{
+ 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,() =>{
     console.log(`Server is listening at http://localhost:${PORT}`)
-    await connectdb();
 });
+
 
 module.exports = app;
